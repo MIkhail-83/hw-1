19c864b21c099c8e91e4d34805a475b9b51f8452
Mikhail-83 <odinsk111@yandex.ru>
2021-06-23 01:24:51 +0800

    homework1

--- src/lesson1/HomeWorkApp.java
+++ src/lesson1/HomeWorkApp.java
@@ -2,6 +2,54 @@ package lesson1;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-        System.out.println("111");
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 5;
+        int b = 8;
+
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = -151;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 1 && value <= 100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 72;
+        int b = 58;
+        if (a >= b) {
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<=b");
+        }
     }
 }
+
+
+
+
